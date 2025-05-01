fc87323d293c12b2c129d55f495caff5e5e94349
Rifqy Attaufi <rifqy1406@gmail.com>
2025-05-01 19:53:37 +0700

    add luas on unit budidaya

--- src/routes/farm/jenisBudidaya.js
+++ src/routes/farm/jenisBudidaya.js
@@ -1,22 +1,36 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const JenisBudidayaController = require('../../controller/farm/jenisBudidaya.js');
-const auditMiddleware = require('../../middleware/auditTrail.js');
+const JenisBudidayaController = require("../../controller/farm/jenisBudidaya.js");
+const auditMiddleware = require("../../middleware/auditTrail.js");
 
 const sequelize = require("../../model/index");
 const db = sequelize.sequelize;
 const JenisBudidaya = sequelize.JenisBudidaya;
 
-router.get('/', JenisBudidayaController.getAllJenisBudidaya);
+router.get("/", JenisBudidayaController.getAllJenisBudidaya);
 
-router.get('/:id', JenisBudidayaController.getJenisBudidayaById);
+router.get("/:id", JenisBudidayaController.getJenisBudidayaById);
 
-router.get('/search/:nama', JenisBudidayaController.getJenisBudidayaByName);
+router.get("/search/:nama", JenisBudidayaController.getJenisBudidayaByName);
 
-router.post('/', auditMiddleware({ model: JenisBudidaya, tableName: "JenisBudidaya" }), JenisBudidayaController.createJenisBudidaya);
+router.post(
+  "/",
+  auditMiddleware({ model: JenisBudidaya, tableName: "JenisBudidaya" }),
+  JenisBudidayaController.createJenisBudidaya
+);
 
-router.put('/:id', auditMiddleware({ model: JenisBudidaya, tableName: "JenisBudidaya" }), JenisBudidayaController.updateJenisBudidaya);
+router.put(
+  "/:id",
+  auditMiddleware({ model: JenisBudidaya, tableName: "JenisBudidaya" }),
+  JenisBudidayaController.updateJenisBudidaya
+);
 
-router.delete('/:id', auditMiddleware({ model: JenisBudidaya, tableName: "JenisBudidaya" }), JenisBudidayaController.deleteJenisBudidaya);
+router.delete(
+  "/:id",
+  auditMiddleware({ model: JenisBudidaya, tableName: "JenisBudidaya" }),
+  JenisBudidayaController.deleteJenisBudidaya
+);
+
+router.get("/tipe/:tipe", JenisBudidayaController.getJenisBudidayaByTipe);
 
 module.exports = router;
