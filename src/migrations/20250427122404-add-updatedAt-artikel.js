b0593ccaf6aa31e006c3de5add54055f405957cc
Rifqy Attaufi <rifqy1406@gmail.com>
2025-05-04 23:31:29 +0700

    fix migration

--- src/migrations/20250427122404-add-updatedAt-artikel.js
+++ src/migrations/20250427122404-add-updatedAt-artikel.js
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    await queryInterface.addColumn('Artikel', 'updatedAt', {
+    await queryInterface.addColumn('artikel', 'updatedAt', {
       type: Sequelize.DATE,
       allowNull: false,
       defaultValue: Sequelize.literal('CURRENT_TIMESTAMP'),
@@ -11,6 +11,6 @@ module.exports = {
   },
 
   async down(queryInterface, Sequelize) {
-    await queryInterface.removeColumn('Artikel', 'updatedAt');
+    await queryInterface.removeColumn('artikel', 'updatedAt');
   }
 };
