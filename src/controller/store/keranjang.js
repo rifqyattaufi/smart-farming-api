77d3c363520a3920ca8f86aab927fd949db1fba1
abrielkarisma <101442361+abrielkarisma@users.noreply.github.com>
2025-05-08 13:33:41 +0700

    fix

--- src/controller/store/keranjang.js
+++ src/controller/store/keranjang.js
@@ -8,25 +8,25 @@ const createKeranjang = async (req, res) => {
         const { id_produk, jumlah } = req.body;
         const { error } = dataValid(req.body, "createKeranjang");
         if (error) {
-        return res.status(400).json({
-            message: error.details[0].message,
-        });
+            return res.status(400).json({
+                message: error.details[0].message,
+            });
         }
-    
+
         const data = await Keranjang.create({
-        id_produk,
-        jumlah,
-        UserId: req.user.id,
+            produkId,
+            jumlah,
+            UserId: req.user.id,
         });
-    
+
         return res.status(201).json({
-        message: "Successfully added to cart",
-        data: data,
+            message: "Successfully added to cart",
+            data: data,
         });
     } catch (error) {
         res.status(500).json({
-        message: error.message,
-        detail: error,
+            message: error.message,
+            detail: error,
         });
     }
 }
