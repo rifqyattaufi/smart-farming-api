ff847c59fc717973575f8d99acdb110fdc05c5e5
Rifqy Attaufi <rifqy1406@gmail.com>
2025-06-13 20:55:45 +0700

    manual fix to startup.sh

--- startup.sh
+++ startup.sh
@@ -7,7 +7,8 @@ echo "--- Running startup.sh script ---"
 
 # 1. Jalankan migrasi database dengan memanggil file biner secara langsung
 echo "Running database migrations..."
-./node_modules/.bin/sequelize db:migrate
+npm i
+# ./node_modules/.bin/sequelize db:migrate
 
 # 2. Jika migrasi berhasil, skrip akan lanjut ke sini.
 echo "Migrations completed successfully."
