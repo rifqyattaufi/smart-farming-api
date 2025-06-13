2bad6d3b5b13ea12d0c3e667e0a311090a3e22b7
Rifqy Attaufi <rifqy1406@gmail.com>
2025-06-13 22:42:08 +0700

    Adjust SSL config for Azure environment

--- src/config/config.js
+++ src/config/config.js
@@ -31,7 +31,7 @@ module.exports = {
     dialectOptions: {
       ssl: {
         require: true,
-        rejectUnauthorized: true,
+        rejectUnauthorized: false,
       },
     },
     // dialectOptions: {
