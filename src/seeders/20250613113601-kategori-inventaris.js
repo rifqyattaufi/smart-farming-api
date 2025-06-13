74600a86902b184c35f60e5b5e2f874fada8de53
Rifqy Attaufi <rifqy1406@gmail.com>
2025-06-13 23:09:55 +0700

    update kategori inventaris seeder

--- src/seeders/20250613113601-kategori-inventaris.js
+++ src/seeders/20250613113601-kategori-inventaris.js
@@ -16,21 +16,25 @@ module.exports = {
       "kategoriInventaris",
       [
         {
+          id: "ac3a537e-486f-4f99-ace0-e398765bcd0d",
           nama: "Vitamin",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          id: "082802a5-54ba-470f-925e-f90ff6ad447f",
           nama: "Vaksin",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          id: "08bf7193-7e3b-4a15-ae1a-ff273833fff0",
           nama: "Pupuk",
           createdAt: new Date(),
           updatedAt: new Date(),
         },
         {
+          id: "1dd42017-1358-4141-859c-5084f347f534",
           nama: "Disinfektan",
           createdAt: new Date(),
           updatedAt: new Date(),
